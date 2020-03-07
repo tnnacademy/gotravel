1af81dd557f6648dd17088bcaf89a97cff968e91
theandersonn <theandersonn@gmail.com>
2020-03-07 17:22:23 -0300

    Ajusta posicionamento section-header [mobile]

--- _src/js/inc/anchor.js
+++ _src/js/inc/anchor.js
@@ -1,11 +1,11 @@
 (function ($) {
   // menu
-  $('.menu > li').on('click', function (event) {
+  $('.menu > li, .menu-mobile > li').on('click', function (event) {
     event.preventDefault();
     
     var ind = $(this).index();
     var roll = $('.anchor').eq(ind).offset().top - 100;
-    
+
     $('html, body').animate({
       scrollTop: roll
     }, 400);
